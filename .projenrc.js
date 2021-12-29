5d35fe8180b2c4801c3ab65ff6ed485b5ac1ee7a
Erno Vuori <erno.vuori@gmail.com>
2021-12-29 13:28:59 +0200

    feat: Add npmAccess and description

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,21 @@
 const { cdk } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new cdk.JsiiProject({
   author: 'Erno Vuori',
   authorAddress: 'erno.vuori@gmail.com',
   defaultReleaseBranch: 'main',
-  name: 'aws-cdk-templates',
+  name: '@rocktemplates/aws-cdk-templates',
   repositoryUrl: 'https://github.com/evuori/aws-cdk-templates.git',
+  description: 'Collection of awesome project types for setting up AWS CDK-based apps with Typescript',
+  npmAccess: NpmAccess.PUBLIC,
+  peerDeps: [
+    'projen',
+  ],
+  keywords: [
+    'aws',
+    'cdk',
+    'projen',
+    'typescript',
+  ],
 });
 project.synth();
