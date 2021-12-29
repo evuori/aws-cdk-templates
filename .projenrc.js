5825413d934f5db6dd3461789abb2b47caf8820d
Erno Vuori <erno.vuori@gmail.com>
2021-12-29 12:49:39 +0200

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { cdk } = require('projen');
+const project = new cdk.JsiiProject({
+  author: 'Erno Vuori',
+  authorAddress: 'erno.vuori@gmail.com',
+  defaultReleaseBranch: 'main',
+  name: 'aws-cdk-templates',
+  repositoryUrl: 'https://github.com/erno.vuori/aws-cdk-templates.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // release: undefined,      /* Add release management to this project. */
+});
+project.synth();
